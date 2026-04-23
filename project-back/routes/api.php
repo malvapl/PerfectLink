095048b3f53b90810937603d94649a471f8b948d
Malva <malvaorbitadigital@gmail.com>
2026-04-23 17:18:58 +0200

    Update api.php

--- project-back/routes/api.php
+++ project-back/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\GuestApiController;
 use App\Http\Controllers\TableApiController;
 use App\Http\Controllers\UserApiController;
@@ -9,16 +8,6 @@ use App\Http\Controllers\WeddingApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the 'api' middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
    return $request->user();
@@ -30,12 +19,10 @@ Route::get('users/weddings', [UserApiController::class, 'weddings']);
 Route::get('userRole/{wedding}', [UserApiController::class, 'roleWedding']);
 Route::get('checkAdmin', [UserApiController::class, 'isAdmin']);
 
-
 // register login
 Route::post('registro', [UserApiController::class, 'register'])->name('registro');
 Route::post('login', [UserApiController::class, 'login'])->name('login');
 Route::get('searchEmail/{email}', [VerificationApiController::class, 'searchEmail']);
-
 
 // create wedding
 Route::post('weddings', [WeddingApiController::class, 'store']);
@@ -45,12 +32,9 @@ Route::get('users/joinWedding/{code}', [UserApiController::class, 'joinWedding']
 Route::get('wedding/codeGuest/{code}', [UserApiController::class, 'existsCodeGuest']);
 Route::get('wedding/codeOrg/{code}', [UserApiController::class, 'existsCodeOrg']);
 
-
-
 // GUEST ONLY
 Route::post('users/confirmInvite/{wedding}', [GuestApiController::class, 'confirmInvite']);
 Route::post('users/cancelInvite/{wedding}', [GuestApiController::class, 'cancelInvite']);
-
 
 // ORGANIZER ONLY
 Route::patch('weddings/{wedding}', [WeddingApiController::class, 'update']);    // ->middleware(['auth:sanctum', 'abilities:patch-wedding']);
@@ -85,8 +69,6 @@ Route::get('weddings', [WeddingApiController::class, 'index']);
 Route::delete('weddings/{wedding}', [WeddingApiController::class, 'destroy']);  // ->middleware(['auth:sanctum', 'abilities:delete  -wedding', 'role:admin']);
 
 
-
-Route::fallback(function(){
-   return response()->json([
-       'message' => 'Page Not Found. If error persists, contact info@website.com'], 404);
+Route::fallback(function () {
+   return response()->json(['message' => 'Page Not Found'], 404);
 });
