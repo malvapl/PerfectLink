9912b950d05240e9ec79c82d0c6c83478d3ee1ae
Malva <malvaorbitadigital@gmail.com>
2026-04-24 13:20:12 +0200

    Update User.php
    Remove unnecessary docs

--- project-back/app/Models/User.php
+++ project-back/app/Models/User.php
@@ -13,36 +13,8 @@ class User extends Authenticatable
 
     use HasApiTokens, HasFactory, Notifiable, HasRoles, HasApiTokens;
 
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'lastname',
-        'email',
-        'password'
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-        'created_at',
-        'updated_at'
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+    protected $fillable = ['name', 'lastname', 'email', 'password'];
+    protected $hidden = ['password', 'remember_token', 'created_at', 'updated_at'];
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
