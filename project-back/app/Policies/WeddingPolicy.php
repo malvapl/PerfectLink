abf498e9cc9eaaee3193b968d49fe9de19f0e957
Malva <malvaplez@gmail.com>
2026-05-06 12:35:33 +0200

    Create WeddingPolicy.php

---
+++ project-back/app/Policies/WeddingPolicy.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Wedding;
+
+class WeddingPolicy
+{
+    /**
+     * Admin only
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Organizer, guestPending, guestConfirmed
+     */
+    public function view(User $user, Wedding $wedding): bool
+    {
+        return $this->hasWeddingRole($user, $wedding, [1, 2, 3]);
+    }
+
+    /**
+     * Check if user is already an organizer of another wedding
+     */
+    public function create(User $user): bool
+    {
+        return !$user->weddings()
+            ->wherePivot('role_id', 1)
+            ->exists();
+    }
+
+    /**
+     * Organizer only
+     */
+    public function update(User $user, Wedding $wedding): bool
+    {
+        return $this->hasWeddingRole($user, $wedding, [1]);
+    }
+
+    /**
+     * Admin or organizer
+     */
+    public function delete(User $user, Wedding $wedding): bool
+    {
+        return $user->is_admin || $this->hasWeddingRole($user, $wedding, [1]);
+    }
+
+    /**
+     * Check if user has any of the specified roles for the wedding.
+     */
+    private function hasWeddingRole(User $user, Wedding $wedding, array $roleIds): bool
+    {
+        return $user->weddings()
+            ->where('wedding_id', $wedding->id)
+            ->whereIn('role_id', $roleIds)
+            ->exists();
+    }
+}
