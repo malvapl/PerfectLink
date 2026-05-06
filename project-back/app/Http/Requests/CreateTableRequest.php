12822df23e0f4ef004392b0003b1dbd559ebd4f2
Malva <malvaplez@gmail.com>
2026-05-06 12:55:06 +0200

    Reformulation
    Remove unnecessary function docs

--- project-back/app/Http/Requests/CreateTableRequest.php
+++ project-back/app/Http/Requests/CreateTableRequest.php
@@ -16,11 +16,6 @@ class CreateTableRequest extends FormRequest
         return $this->user()->hasOwnWedding() == $weddingId;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
--- project-back/app/Http/Requests/CreateWeddingRequest.php
+++ project-back/app/Http/Requests/CreateWeddingRequest.php
@@ -7,18 +7,13 @@ use Illuminate\Foundation\Http\FormRequest;
 class CreateWeddingRequest extends FormRequest
 {
    /**
-    * Determine if the user is authorized to make this request.
+    * Checked in policy
     */
    public function authorize(): bool
    {
       return true;
    }
 
-   /**
-    * Get the validation rules that apply to the request.
-    *
-    * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-    */
    public function rules(): array
    {
       return [
